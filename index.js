80adcd400ae29484eba49c8fa38ddf7e4520b013
kach0w <karsab343@gmail.com>
2023-04-23 19:28:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,9 @@ const app = express();
 const port = 3000;
 const readline = require('readline');
 const fs = require('fs');
-const FILE_PATH = 'data.ndjson';
-var requestIp = require('request-ip');
-const {spawn} = require('child_process');
-// app.get('/', (req, res) => {
-//    var dataToSend;
-//    // spawn new child process to call the python script
-//    const python = spawn('python', ['sendEmail.py']);
-//    // collect data from script
-//    python.stdout.on('data', function (data) {
-//     console.log('Pipe data from python script ...');
-//     dataToSend = data.toString();
-//    });
-//    // in close event we are sure that stream from child process is closed
-//    python.on('close', (code) => {
-//      console.log(`child process close all stdio with code ${code}`);
-//      // send data to browser
-//      res.send(dataToSend)
-//    });
-// })
+const fetch = require('node-fetch');
+
 app.get('/', (req, res) => res.send('Online'));
-app.get('/',function(req, res) {
-   
-    var clientIp = requestIp.getClientIp(req);
-    console.log(clientIp);
-   
-});
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 // ================= START BOT CODE ===================
@@ -36,179 +13,127 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const activities_list = [
-    { type: 'PLAYING',  message: 'Commands list - $commands'  },
-    { type: 'WATCHING', message: 'Pie!' },
-    { type: 'STREAMING', message: 'Lightning McQueen' }
+    { type: 'PLAYING',  message: 'Kindly!'},
+    { type: 'WATCHING', message: 'Positivity!'}
 ];
 
 client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-
         client.user.setActivity(activities_list[index].message, { type: activities_list[index].type });
     }, 10000);
 });
+//data
+var myDelayedData = '';
 
 client.on('messageCreate', msg => {
-  //app.get("/", function (req, res) { //prints ip
-    // let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-    // res.send(ip)
   
-  //}); 
-  // var getIP = require('ipware')().get_ip;
-  // var ip = function(req, res){
-  // console.log(ip)
-
-  // ip();
-  // console.log(getIP)
-  if (msg.content === 'ping') {
-    msg.reply('pong!');
-     permissionOverwrites: [
-        {
-            id: guild.me.roles.highest,
-            allow: ['VIEW_CHANNEL', 'MANAGE_CHANNELS', 'MANAGE_ROLES']
-        }
-    ]
-  }
-  if(msg.content.includes("$astronaut") || msg.content.includes("astronaut") || msg.content.includes("astronaut in the ocean")){
-    msg.channel.send("https://i.imgur.com/76aiE5h.jpeg");
-  }
-  if (msg.content == 'karthik' || msg.content == 'Karthik' || msg.content == '$Karthik' || msg.content == '$karthik'){
-    n =  new Date();
-    y = n.getFullYear();
-    m = n.getMonth()+1;
-    d = n.getDate()-1;
-    h = n.getHours()+5;
-    min = n.getMinutes();
-    s = n.getSeconds();
-    day = n.getDay();
-    msg.reply('Who dares say the name of the might Karthik!');
-    console.log(`${msg.author.tag} dared to use the name of the Great Karthik`);
-    console.log("On: " + m + "/" + d + "/" + y + " At time: " + h + ":" + min + ":" + s)
+  async function checkIfNegative(msg){
+    const message = msg.content;
+    const encodedParams = new URLSearchParams();
+    encodedParams.set('text', message);
     
-  }
-  
-  if(msg.content == '$yourebad'){
-    n =  new Date();
-    y = n.getFullYear();
-    m = n.getMonth()+1;
-    d = n.getDate()-1;
-    h = n.getHours()+5;
-    min = n.getMinutes();
-    s = n.getSeconds();
-    day = n.getDay();
-    for(let i=0; i<4; i++){
-      //msg.channel.send('@everyone');
-    }
-    msg.channel.send('You are bad');
-    console.log(`${msg.author.tag} is bad`);
-    console.log("On: " + m + "/" + d + "/" + y + " At time: " + h + ":" + min + ":" + s)
-  }
-  
-  if(msg.content == ('$question') || msg.content ==('$rickroll') || msg.content == 'rick' || msg.content == 'rickroll' ){
-    n =  new Date();
-    y = n.getFullYear();
-    m = n.getMonth()+1;
-    d = n.getDate()-1;
-    h = n.getHours()+5;
-    min = n.getMinutes();
-    s = n.getSeconds();
-    day = n.getDay();
-    msg.channel.send('https://imgur.com/gallery/Gc9O0Xy');
-    console.log(`${msg.author.tag} is getting rickrolled`);
-    console.log("On: " + m + "/" + d + "/" + y + " At time: " + h + ":" + min + ":" + s)
-  }
-  
-  if(msg.content == '$commands'){
-    n =  new Date();
-    y = n.getFullYear();
-    m = n.getMonth()+1;
-    d = n.getDate()-1;
-    h = n.getHours()+5;
-    min = n.getMinutes();
-    s = n.getSeconds();
-    day = n.getDay();
-    msg.channel.send("https://docs.google.com/document/d/1L2UE-9tP9GCC76iIwYD-OqsPrx3VtcmjzFu3lZRV0co/edit?usp=sharing");
-    console.log(`${msg.author.tag} is looking for the commands list`);
-    console.log("On: " + m + "/" + d + "/" + y + " At time: " + h + ":" + min + ":" + s)
-  }
-  
-  if(msg.content == '$spammy'){
-    for(let i=0; i<10; i++){
-      //return "everyone";
-      //msg.channel.send("@everyone");   
+    const url = 'https://text-sentiment.p.rapidapi.com/analyze';
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': '0a110d7d80msh1aee9c56803a5d5p1401c3jsn90f55ffc86dd',
+        'X-RapidAPI-Host': 'text-sentiment.p.rapidapi.com'
+      },
+      body: encodedParams
+    };
+    try {
+    	const response = await fetch(url, options);
+    	const result = await response.text();
+      let json = JSON.parse(result);
+      console.log(json)
+      if(json.neg_percent === "0%"){
+        return await 0;
+      }
+      let neg = json.neg_percent.substring(0, json.neg_percent.length - 1)
+      // console.log(neg)
+      if(Number(neg) > 50){
+        return await Number(neg);
+      } else {
+        return await 0;
+      }
+    } catch (error) {
+    	console.error(error);
     }
   }
+  // async function getUserMessages(guildID, userID){
+  //   let average = 0
+  //   const arr = []
+  //   let ch = msg.guild.channels.cache.find(channel => channel.name === 'general');
+  //   ch.messages.fetch({
+  //       limit: 5
+  //   }).then(messages => {
+  //       const msgs = messages.filter(m => m.author.id === userID)
+  //       msgs.forEach(m => {
+  //           arr.push(checkIfNegative(m))
+  //       })
+  //       let sum = 0;
+  //       let avg = 0;
+          
+         
+        
+  //       // const values = getArr()
+  //       // const total = values.reduce((p, c) => p + c);
+  //       // return total / 5;
   
-  if(msg.content == 'avocado craft'){
-    msg.channel.send('https://www.youtube.com/watch?v=uRiGdl_fD28');
+  //   })
+     
+  //     Promise.all(arr).then((results) => {
+  //       const total = results.reduce((p, c) => p + c);
+  //       return total/5;
+  //     });
+      
+  //   }
+  if(msg.content.includes('$check') & msg.mentions.members.size) {
+    const user = msg.mentions.users.first() || msg.guilds.members.cache.get(args[0])?.user;
+    const userId = user.id;
+    const guildId = "992101058375581716";
+    
+    async function getUserMessages(guildID, userID){
+      let average = 0
+      const arr = []
+      let ch = msg.guild.channels.cache.find(channel => channel.name === 'general');
+      ch.messages.fetch({
+          limit: 50
+      }).then(messages => {
+          const msgs = messages.filter(m => m.author.id === userID)
+          msgs.forEach(m => {
+              arr.push(checkIfNegative(m))
+          })
+          let sum = 0;
+          let avg = 0;
+          Promise.all(arr).then((results) => {
+            const total = results.reduce((p, c) => p + c);
+            const avg = total/5
+            console.log(avg)
+            if(avg > 50){
+              msg.reply("This user needs to be more positive.")
+            } else {
+              msg.reply("This user seems to be quiet positive!")
+            }
+          });
+    
+      })
+    }
+    getUserMessages(guildId, userId)
+
   }
   
-  if(msg.content.includes('$dm') & msg.mentions.members.size){
-    n =  new Date();
-    y = n.getFullYear();
-    m = n.getMonth()+1;
-    d = n.getDate()-1;
-    h = n.getHours()+5;
-    min = n.getMinutes();
-    s = n.getSeconds();
-    day = n.getDay();
+  
+  if(msg.content.includes('$comfort') & msg.mentions.members.size){
     const args = msg.content.split(' ')
     const user = msg.mentions.users.first() || msg.guilds.members.cache.get(args[0])?.user;
-    const str = args.slice(1).join(" ");
-    if(msg.content.includes('anon')){
-      //when people change their usernames update here
-      if(user.username == 'kachow' || user.username=='bobbob456' || user.username == 'monkey_boi'){
-        user.send(`${msg.author.tag} is trying to send you an anonymous message:`)
-      }
-      user.send(str.replace("anon", "").replace(user, ""));
-    }
-    else{
-      user.send(`${msg.author.tag} says${str.replace(user, "")}`);
-    }
-    console.log(`${msg.author.tag} is sending a message to ${user.username}`);
-    console.log("On: " + m + "/" + d + "/" + y + " At time: " + h + ":" + min + ":" + s)
+    // const str = args.slice(1).join(" ");
+    const str = "Here are some tips to stay more positive: \n \n Practice gratitude: Make a habit of focusing on the things you're thankful for. Start each day by listing a few things you're grateful for and regularly reflect on the blessings in your life. \n \n Surround yourself with positive people: Spend time with people who uplift you and encourage you to be your best self. Avoid negative people who bring you down. \n  Find activities that make you happy: Engage in activities that bring you joy and fulfillment. It could be anything from reading to dancing, cooking to playing sports.\n \n Focus on the present moment: Instead of dwelling on the past or worrying about the future, focus on the present moment and enjoy it fully. \n \n Practice positive self-talk: Replace negative self-talk with positive affirmations. Speak kindly to yourself and focus on your strengths."
+    user.send(`${msg.author.tag} says ${str.replace(user, "")}`);
   }
   
-  if(msg.content == '$funnywebsite'){
-    n =  new Date();
-    y = n.getFullYear();
-    m = n.getMonth()+1;
-    d = n.getDate()-1;
-    h = n.getHours()+5;
-    min = n.getMinutes();
-    s = n.getSeconds();
-    day = n.getDay();
-    const randomLineIndex = Math.floor(Math.random() * 23);
-    //console.log(randomLineIndex);
-    const websites = [
-    "https://thatsthefinger.com/",
-    "https://cant-not-tweet-this.com/",
-    "https://cat-bounce.com/",
-    "https://www.internetlivestats.com/",
-    "http://www.staggeringbeauty.com/",
-    "https://isitchristmas.com/",
-    "http://ww12.beesbeesbees.com/",
-    "http://endless.horse/",
-    "http://corndog.io/",
-    "https://trypap.com/",
-    "http://www.everydayim.com/",
-    "https://hackertyper.com/",
-    "https://www.trashloop.com/",
-    "https://pixelsfighting.com/",
-    "http://wowenwilsonquiz.com/",
-    "http://papertoilet.com/",
-    "https://willrobotstakemyjob.com/",
-    "https://zoomquilt.org/",
-    "http://make-everything-ok.com/",
-    "https://findtheinvisiblecow.com/",
-    "https://printer.discord.com/",
-    "https://printer.discord.com/",
-    "https://www.nyan.cat/"];
-    msg.channel.send(websites[randomLineIndex]);
-    console.log(`${msg.author.tag} is showing a funny website in ${msg.channel}`);  
-    console.log("On: " + m + "/" + d + "/" + y + " At time: " + h + ":" + min + ":" + s)
-  }
 });
 
 
